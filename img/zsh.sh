acd54574c2daffb0f00ca30402e1898ec4ec7307
Caitlin Elfring <celfring@gmail.com>
2020-09-02 22:23:02 -0400

    Better zsh prompt for gif

--- img/zsh.sh
+++ img/zsh.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
-export PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ ) %{$fg[cyan]%}~%{$reset_color%} "
 export ZSH=~/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
+export PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ ) "
+PROMPT+="%{$fg[cyan]%}woke%{$reset_color%} %{$fg_bold[blue]%}git:(%{$fg[red]%}main%{$fg[blue]%})%{$reset_color%} "
 exec zsh -d -f "$@"
