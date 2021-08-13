ccc914081a484cd18351c802d5a5d16392c7d640
Caitlin Elfring <celfring@gmail.com>
2021-08-13 09:47:54 -0400

    Make sure the go downloaded version of woke is used

--- img/zsh.sh
+++ img/zsh.sh
@@ -2,6 +2,7 @@
 
 export ZSH=~/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
+export PATH="$GOPATH/bin:$PATH"
 
 export PROMPT="%(?:%{$fg_bold[green]%}➜ :%{$fg_bold[red]%}➜ ) "
 PROMPT+="%{$fg[cyan]%}woke%{$reset_color%} %{$fg_bold[blue]%}git:(%{$fg[red]%}main%{$fg[blue]%})%{$reset_color%} "
